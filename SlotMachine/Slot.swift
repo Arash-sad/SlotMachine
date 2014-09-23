147f5013376dcfc24d238f3843133709884c46ef
Arash Sadeghieh <Arash@Arashs-MacBook-Pro.local>
2014-09-23 22:12:34 +1000

    WTF

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,18 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Arash Sadeghieh E on 20/09/2014.
+//  Copyright (c) 2014 Arash Sadeghieh Eshtehadi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image:UIImage = UIImage(named: "Ace")
+    var isRed = true
+}
+
+
